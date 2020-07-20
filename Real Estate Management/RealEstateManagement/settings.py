fb6276fedf1547d371de39f7c8193c0ca3c5d364
Krishna Patil <krishnapatil1406@gmail.com>
2020-07-20 10:58:21 +0530

    Update settings.py

--- Real Estate Management/RealEstateManagement/settings.py
+++ Real Estate Management/RealEstateManagement/settings.py
@@ -4,7 +4,7 @@ import os
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 't#9_#6f*0*qsfm_8l5v$jj*ut@og*^ayf3r%mr8)og8xg+wq0p'
+SECRET_KEY = 'your_secret_key_here'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
